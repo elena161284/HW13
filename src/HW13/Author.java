eb039a0a745191a2d6d03b7cf178b3f1bf0c5c15
Elena <elena161284@mail.ru>
2023-05-30 10:10:41 +0300

    HW13

---
+++ src/HW13/Author.java
@@ -0,0 +1,12 @@
+package HW13;
+
+    public class Author{
+        private final String name;
+        private final String surName;
+        public Author (String name, String surName) {
+            this.name = name;
+            this.surName = surName;
+        }
+        public String getName() {return this.name;}
+        public String getSurName() {return this.surName;}
+    }
